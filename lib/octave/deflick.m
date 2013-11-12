97cfd4a0dc415fd2f98a32961083a839952f5587
alex.jullien@gmail.com <alex.jullien@gmail.com@6529f8e2-aedb-8472-bb65-d41144f03e13>
2013-11-12 16:23:36 +0000

    deflick.m info and update

--- lib/octave/deflick.m
+++ lib/octave/deflick.m
@@ -1,13 +1,25 @@
+% Note that this file is given for information (or debug purpose on octave)
+% the source file used in timelapse-darktable is in deflick package
+% class : WriteOctaveDeflickFcn 
+% To modify the source : modify WriteOctaveDeflickFcn.java
+
 function deflick(luminanceTableFic)
 % compute deflickering parameters from luminance table
 
 % retrieve source luminance table
 in_signal = load(luminanceTableFic);
+in_signal = in_signal(:); % column
 
-% filtering parameters
+% first : coarse filtering with window averaging
+[avgSignal] = windowAverage(in_signal,24);
+
+% standard variation of luminance wrt average
+stdLum = std(avgSignal-in_signal);
+
+% advanced filtering parameters
 N_LpFilt    = min(50,floor(length(in_signal)/2));   % Window size for filtering
 N_SpikeFilt = min(12,floor(length(in_signal)/4+1)); % Size of filtered spike
-dL_max      = max(0.05/2,3*std(in_signal));         % 'normal' deltaL max (above, it is considered as a spike)
+dL_max      = max(0.02,1.5*stdLum);           % 'normal' deltaL max (above, it is considered as a spike)
 isPlotFlag  = true;                                 % output a grapics with deflickering source/target curves
 
 % filtering (deflickering target)
@@ -44,32 +56,17 @@ N_SpikeFilt=min(N_SpikeFilt,Ntot);
 % -------------------------------
 in_time=1:Ntot;
 notRejIdx=1:Ntot;
-filtSpikes_signal=in_signal;
-for iShift=1:N_SpikeFilt
-    % diff N / N-iShift
-    diffSignal=in_signal-in_signal([ones(1,iShift) 1:(end-iShift)]);
-    % not rejected if never rejected:
-    notRejIdx=intersect(notRejIdx,find(diffSignal<dL_max));
+[avgSignal] = windowAverage(in_signal,24);
+notRejIdx = find(abs(avgSignal-in_signal)<dL_max);
+if length(notRejIdx)<2
+ % keep 1st and last index
+ notRejIdx = unique([1 notRejIdx(:)' length(notRejIdx)]);
 end
 % delete spikes and replace them by linear interpolated values
-filtSpikes_signal=interp1(in_time(notRejIdx),filtSpikes_signal(notRejIdx),in_time,'linear','extrap');
+filtSpikes_signal=interp1(in_time(notRejIdx),in_signal(notRejIdx),in_time,'linear','extrap');
 
-
-% FILTERING SIGNAL
-% ----------------
-% window and weights before/after current sample
-weightGen=abs([1./[-N_LpFilt:-1] 1 1./[1:N_LpFilt]].^0.5);
-winGen=(-N_LpFilt):(N_LpFilt);
-% apply filter on a sliding window
-filt_signal=zeros(1,Ntot);
-for iL=1:Ntot
-	idx = intersect(find((winGen+iL)>1),find((winGen+iL)<Ntot));
-	winIdx = iL+winGen(idx);
-	weight=weightGen(idx);
-	weigthTot=sum(weight);
-	% weighted averaging on current window samples
-	filt_signal(iL) = sum(filtSpikes_signal(winIdx).*weight/weigthTot);
-end
+% final window-averaging 
+filt_signal = windowAverage(in_signal,N_LpFilt);
 
 % PLOT
 % ----
@@ -88,11 +85,31 @@ end
 
 end
 
+function out_avg = windowAverage(in_raw,winHalfSize)
+
+% compute the average of the signal on the window size
+Ntot=length(in_raw);
+% window and weights before/after current sample
+weightGen=abs([1./[-winHalfSize:-1] 1 1./[1:winHalfSize]].^0.5);
+winGen=(-winHalfSize):(winHalfSize);
+% apply filter on a sliding window
+out_avg=zeros(size(in_raw));
+for iL=1:Ntot
+	idx = intersect(find((winGen+iL)>1),find((winGen+iL)<Ntot));
+	winIdx = iL+winGen(idx)-1;
+	weight=weightGen(idx);
+	weigthTot=sum(weight);
+	% weighted averaging on current window samples
+	% keyboard;
+	out_avg(iL) = sum(in_raw(winIdx).*weight(:)/weigthTot);
+end
+
+
 function printTable(filename,values)
 % print in a text file
 
 fid=fopen(filename,'w');
 for iVal=1:length(values)
-	fprintf(fid,'%.6f\n',values(iVal));
+	fprintf(fid,'%.6f\\n',values(iVal));
 end
 fclose(fid);
